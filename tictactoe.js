f0a13a80dd793d2e4a0a3700aea3d48bc97883aa
Rajon Bhuiyan <bhuiyan.rajon@gmail.com>
2021-08-31 17:25:27 -0700

    created function stubs for the most important modules and factories

---
+++ tictactoe.js
@@ -0,0 +1,13 @@
+//stub for board
+const board = (() => {
+    //create a 2d array that holds the game board
+    var gameboard = [[0,0,0],[0,0,0],[0,0,0]];
+    const render =() => {
+        //code for rendering the board
+    };
+})();
+
+//stub for the player factory method
+const player = ((name) => {
+    var score = 0;
+});
